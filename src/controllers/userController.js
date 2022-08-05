4f72b2650afc9875c79240d9644622978b0dcc98
In Lee <leein9404@naver.com>
2022-08-05 18:26:35 +0900

    github login is not working

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -102,12 +102,53 @@ export const finishGithubLogin = async (req, res) => {
     }
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    const data = await fetch(finalUrl, {
+    const tokenRequest = await (await fetch(finalUrl, {
         method:"POST",
         headers:{
             Accept: "application/json",
         }
-    })
-    const json = await data.json();
-    console.log(json)
+    })).json();
+    if ("access_token" in tokenRequest) {
+        const {access_token} = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (await fetch(`${apiUrl}/user`, {
+            headers:{
+                Authorization: `token ${access_token}`
+            }
+        })).json(); 
+        // console.log(userData);
+        const emailData = await (await fetch(`${apiUrl}/user/emails` ,{
+            headers:{
+                Authorization: `token ${access_token}`
+            }
+        })).json();
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if (!emailObj) {
+            return res.redirect("/login");
+        }
+        const existingUser = await User.findOne({email : emailObj.email});
+
+        if (existingUser) {
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect('/');
+        } else {
+            const user = await User.create({
+                name:userData.name, 
+                username:userData.loggedIn, 
+                email:emailObj.email, 
+                password:"", 
+                location:userData.location,
+                socialOnly:true,
+            });
+            console.log(user);
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect('/');
+        }
+    } else {
+        return res.redirect("/login");
+    }
 }
