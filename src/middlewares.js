dc33cb228fd6fef89cdf522baf48ecf0b93b95e0
In Lee <leein9404@naver.com>
2022-10-24 19:08:02 +0900

    NoSuchBucket error

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,29 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+// const { S3Client } = require('@aws-sdk/client-s3');
+import { S3Client, AbortMultipartUploadCommand } from "@aws-sdk/client-s3";
+
+// const s3 = new S3Client({
+//     region: "ap-northeast-2",
+//     apiVersion: "2022-10-24",
+//     credentials :  {
+//         accessKeyId: process.env.AWS_ID,
+//         secretAccessKey: process.env.AWS_SECRET,
+//     },
+// });
+const s3 = new S3Client({
+    region: "ap-northeast-2",
+    credentials: {
+        apiVersion: "2022-10-24",
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET,
+    },
+});
+
+const multerUploader = multerS3({
+    s3: s3,
+    bucket: 'WetubeReloadedUploader',
+})
 
 export const localMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube";
@@ -28,9 +53,17 @@ export const publicOnlyMiddleware = (req, res, next) => {
 }
 
 // export const uploadFiles = multer({ dest: "uploads/" });
-export const avatarUpload = multer({ dest: "uploads/avatars/", limits :{
-    fileSize : 3000000,
-} });
-export const videoUpload = multer({ dest: "uploads/videos/", limits :{
-    fileSize : 10000000,
-} });
+export const avatarUpload = multer({ 
+    dest: "uploads/avatars/", 
+    limits :{
+        fileSize : 3000000,
+    },
+    storage : multerUploader
+});
+export const videoUpload = multer({ 
+    dest: "uploads/videos/", 
+    limits :{
+        fileSize : 10000000,
+    },
+    storage : multerUploader
+});
